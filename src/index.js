a68669b531952ce68644fb30e0b04ec0817cb0a9
Yulli-Lizano <yulli3lc@gmail.com>
2018-11-20 16:57:02 -0500

    funcion cifrado

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 const cifrar = document.getElementById("cyphing");
-//definimos la funcion que vamos a llamar una vez suceda el evento
-const change = () => {
-  const button1 = document.getElementById("naming").value;
-  const button2 = parseInt(document.getElementById("key").value);
-  //para guardar las letras del mensaje
-  let myString ="";
-  //recorremos el mensaje ingresado
-  for (let i = 0; i < button1.length; i++) {
-  //aplicamcoos la formula y obtenemos la posicion desplazada en nuestro alfabeto
-    let myString2 = (button1.charCodeAt(i)-65+button2)%26+65;
-//pasamos la posicion obtenida a codigo ASCII
+
+function newMessage(texto, number){
+  let myString = "";
+  for (let i = 0; i < texto.length; i++) {
+    let myString2 = (texto.charCodeAt(i)-65+number)%26+65;
     let myString3 = String.fromCharCode(myString2);
-    let myString4 = myString += myString3;
-    document.getElementById("result").innerHTML = myString4;
+    myString += myString3;
   }
 }
-//llamando a la funcion que definimos
+
+const change = () =>{
+  const button1 = document.getElementById("naming").value;
+  const button2 = parseInt(document.getElementById("key").value);
+  const resultado = newMessage(button1, button2);
+  document.getElementById("result").innerHTML = resultado;
+}
 cifrar.addEventListener("click", change);
