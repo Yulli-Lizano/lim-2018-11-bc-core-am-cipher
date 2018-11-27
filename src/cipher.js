1a811b12e2af978f83ffc127a1c7cdd2b66410a0
Yulli-Lizano <yulli3lc@gmail.com>
2018-11-27 16:50:40 -0500

    test unitarios

--- src/cipher.js
+++ src/cipher.js
@@ -1,21 +1,21 @@
 window.cipher = {
   encode:(offset, string)=>{
-  let cipheredLetter = "";
-  for (let i = 0; i < string.length; i++) {
-    let newPosition = ((string.toUpperCase()).charCodeAt(i)-65+offset)%26+65;
-    let cipheredLetter2 = String.fromCharCode(newPosition);
-    cipheredLetter += cipheredLetter2;
-  }
-  return cipheredLetter;
-},
+      let cipheredLetter = "";
+      for (let i = 0; i < string.length; i++) {
+        let newPosition = ((string.toUpperCase()).charCodeAt(i)-65+offset)%26+65;
+        let cipheredLetter2 = String.fromCharCode(newPosition);
+        cipheredLetter += cipheredLetter2;
+      }
+      return cipheredLetter;
+      },
   decode:(offset, string)=>{
-  let decipheredLetter = "";
-  for (let i = 0; i < string.length; i++) {
-    let firstPosition = (string.charCodeAt(i)-90-offset)%26+90;
-    let decipheredLetter2 = String.fromCharCode(firstPosition);
-    decipheredLetter += decipheredLetter2;
-  }
-  return decipheredLetter;
+      let decipheredLetter = "";
+      for (let i = 0; i < string.length; i++) {
+        let firstPosition = (string.charCodeAt(i)-90-offset)%26+90;
+        let decipheredLetter2 = String.fromCharCode(firstPosition);
+        decipheredLetter += decipheredLetter2;
+      }
+      return decipheredLetter;
   }
 
 };
